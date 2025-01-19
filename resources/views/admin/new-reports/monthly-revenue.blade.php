86e01515822abd9325e28da0508c66232c03f23b
Raja Khan <contact.rajakhan@gmail.com>
2025-01-20 01:16:49 +0600

    mouthly revenue data add

--- resources/views/admin/new-reports/monthly-revenue.blade.php
+++ resources/views/admin/new-reports/monthly-revenue.blade.php
@@ -94,114 +94,164 @@
     <script src="https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"></script>
 </head>
 
+@php
+    $start_date = \Illuminate\Support\Carbon::now()->startOfMonth();
+    $end_date = \Illuminate\Support\Carbon::now()->endOfDay();
+
+    $start_date_filter = $start_date;
+    $end_date_filter = $end_date;
+
+    $s_parcels = App\Models\Parcel::whereBetween('parcels.status', [25, 36])
+        ->whereNull('parcels.suborder')
+        ->whereBetween('delivery_branch_date', [$start_date_filter, $end_date_filter])
+        ->whereIn('delivery_type', [1, 2, 4])
+        ->get();
+
+    $total_day = $end_date->diffInDays($start_date);
+
+    $start_date_filter = $start_date->subMonths(1);
+    $end_date_filter = $end_date->subMonths(1);
+
+    $c_parcels = App\Models\Parcel::whereBetween('parcels.status', [25, 36])
+        ->whereNull('parcels.suborder')
+        ->whereBetween('delivery_branch_date', [$start_date_filter, $end_date_filter])
+        ->whereIn('delivery_type', [1, 2, 4])
+        ->get();
+
+    // dd($s_parcels);
+
+@endphp
+
 <body>
     <section style="background: #f8f9fa;" class="print-hidden">
-        <div class="container py-5" style="max-width: 600px; min-width:300px">
-            <div class="d-flex gap-5 wrap">
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Select Mounth</option>
-
-                    <?php
-                    $currentMonth = date('m');
-                    $currentYear = date('Y');
-                    $months = 50;
-                    for ($i = 0; $i < $months; $i++) {
-                        $month = $currentMonth - $i;
-                        $year = $currentYear;
-                        if ($month < 1) {
-                            $month += 12;
-                            $year--;
-                        }
-                        echo '<option value="' . $year . '-' . str_pad($month, 2, '0', STR_PAD_LEFT) . '">' . $year . ' - ' . date('F', mktime(0, 0, 0, $month, 1)) . '</option>';
-                    }
-                    ?>
-
+        <div class="container py-5 mx-auto" style="max-width: 900px;">
+            <form class="d-flex gap-5 flex-wrap justify-content-center flex-lg-nowrap align-content-center">
+                <select name="" id="" class="form-control">
+                    <option value="">All</option>
+                    <option value="">ISD</option>
+                    <option value="">OSD</option>
+                    <option value="">SUB</option>
                 </select>
+                <label for="" style="white-space: nowrap;">Start Date</label>
+                <input type="date" name="start_date" id="" class="form-control"
+                    value="{{ $start_date->format('Y-m-d') }}">
+                <label for="" style="white-space: nowrap;">End Date</label>
+                <input type="date" name="end_date" id="" class="form-control"
+                    value="{{ $end_date->format('Y-m-d') }}">
                 <button type="button" class="btn btn-primary">Filter</button>
-            </div>
+            </form>
         </div>
     </section>
 
     <div class="print-container">
         <div class="label">
-            <h3>Parceldex Limited</h3>
+            <div class="d-flex justify-content-between align-content-end flex-wrap" style="align-items: flex-end;">
+                <h3>Parceldex Limited</h3>
+                <div>
+                    {{ $start_date->format('d M Y') }} - {{ $end_date->format('d M Y') }}
+                </div>
+            </div>
             <table class="table table-bordered table-striped">
                 <thead class="table-dark">
                     <tr>
                         <th></th>
-                        <th>Grand Total</th>
-                        <th>Dec-2024</th>
-                        <th>Nov-2024</th>
+                        <th class="text-end">Grand Total</th>
+                        <th class="text-end">Compare Previous</th>
+                        <th class="text-end">Selected Range</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td><strong>Number of Order</strong></td>
-                        <td>19,921</td>
-                        <td>10,027</td>
-                        <td>9,894</td>
+                        <td class="text-end">{{ $c_parcels->count() + $s_parcels->count() }}</td>
+                        <td class="text-end">{{ $c_parcels->count() }}</td>
+                        <td class="text-end">{{ $s_parcels->count() }}</td>
                     </tr>
                     <tr>
                         <td><strong>Amount to be Collect</strong></td>
-                        <td>35,499,425</td>
-                        <td>17,735,567</td>
-                        <td>17,763,858</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('total_collect_amount') + $s_parcels->sum('total_collect_amount') }}
+                        </td>
+                        <td class="text-end">{{ $c_parcels->sum('total_collect_amount') }}</td>
+                        <td class="text-end">{{ $s_parcels->sum('total_collect_amount') }}</td>
                     </tr>
                     <tr>
                         <td><strong>Collected Amount</strong></td>
-                        <td>30,928,038</td>
-                        <td>14,774,019</td>
-                        <td>16,154,019</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('cancel_amount_collection') + $s_parcels->sum('cancel_amount_collection') + $c_parcels->sum('customer_collect_amount') + $s_parcels->sum('customer_collect_amount') }}
+                        </td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('cancel_amount_collection') + $c_parcels->sum('customer_collect_amount') }}
+                        </td>
+                        <td class="text-end">
+                            {{ $s_parcels->sum('cancel_amount_collection') + $s_parcels->sum('customer_collect_amount') }}
+                        </td>
                     </tr>
                     <tr>
                         <td><strong>Weight Charge</strong></td>
-                        <td>212,190</td>
-                        <td>59,405</td>
-                        <td>152,785</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('weight_package_charge') + $s_parcels->sum('weight_package_charge') }}
+                        </td>
+                        <td class="text-end">{{ $c_parcels->sum('weight_package_charge') }}</td>
+                        <td class="text-end">{{ $s_parcels->sum('weight_package_charge') }}</td>
                     </tr>
                     <tr>
                         <td><strong>COD Charge</strong></td>
-                        <td>117,372</td>
-                        <td>50,369</td>
-                        <td>67,004</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('cod_charge') + $s_parcels->sum('cod_charge') }}
+                        </td>
+                        <td class="text-end">{{ $c_parcels->sum('cod_charge') }}</td>
+                        <td class="text-end">{{ $s_parcels->sum('cod_charge') }}</td>
                     </tr>
                     <tr>
                         <td><strong>Delivery Charge</strong></td>
-                        <td>1,246,020</td>
-                        <td>616,245</td>
-                        <td>629,775</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('delivery_charge') + $s_parcels->sum('delivery_charge') }}
+                        </td>
+                        <td class="text-end">{{ $c_parcels->sum('delivery_charge') }}</td>
+                        <td class="text-end">{{ $s_parcels->sum('delivery_charge') }}</td>
                     </tr>
                     <tr>
                         <td><strong>Return Charge</strong></td>
-                        <td>39,375</td>
-                        <td>14,640</td>
-                        <td>24,735</td>
-                    </tr>
-                    <tr>
-                        <td><strong>Other Charges</strong></td>
-                        <td>0</td>
-                        <td>0</td>
-                        <td>0</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('return_charge') + $s_parcels->sum('return_charge') }}
+                        </td>
+                        <td class="text-end">{{ $c_parcels->sum('return_charge') }}</td>
+                        <td class="text-end">{{ $s_parcels->sum('return_charge') }}</td>
                     </tr>
                     <tr class="table-info">
                         <td><strong>Total Revenue</strong></td>
-                        <td>1,614,957</td>
-                        <td>740,659</td>
-                        <td>874,299</td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('weight_package_charge') + $s_parcels->sum('weight_package_charge') + $c_parcels->sum('cod_charge') + $s_parcels->sum('cod_charge') + $c_parcels->sum('delivery_charge') + $s_parcels->sum('delivery_charge') + $c_parcels->sum('return_charge') + $s_parcels->sum('return_charge') }}
+                        </td>
+                        <td class="text-end">
+                            {{ $c_parcels->sum('weight_package_charge') + $c_parcels->sum('cod_charge') + $c_parcels->sum('delivery_charge') + $c_parcels->sum('return_charge') }}
+                        </td>
+                        <td class="text-end">
+                            {{ $s_parcels->sum('weight_package_charge') + $s_parcels->sum('cod_charge') + $s_parcels->sum('delivery_charge') + $s_parcels->sum('return_charge') }}
+                        </td>
                     </tr>
                 </tbody>
                 <tfoot>
                     <tr>
                         <td><strong>Average Revenue Per Order</strong></td>
-                        <td>81</td>
-                        <td>74</td>
-                        <td>88</td>
+                        <td class="text-end">
+                            {{ number_format(($c_parcels->sum('weight_package_charge') + $s_parcels->sum('weight_package_charge') + $c_parcels->sum('cod_charge') + $s_parcels->sum('cod_charge') + $c_parcels->sum('delivery_charge') + $s_parcels->sum('delivery_charge') + $c_parcels->sum('return_charge') + $s_parcels->sum('return_charge')) / ($c_parcels->count() + $s_parcels->count() ?? 1), 2) }}
+                        </td>
+                        <td class="text-end">
+                            {{ number_format(($c_parcels->sum('weight_package_charge') + $c_parcels->sum('cod_charge') + $c_parcels->sum('delivery_charge') + $c_parcels->sum('return_charge')) / ($c_parcels->count() ?: 1), 2) }}
+                        </td>
+                        <td class="text-end">
+                            {{ number_format(($s_parcels->sum('weight_package_charge') + $s_parcels->sum('cod_charge') + $s_parcels->sum('delivery_charge') + $s_parcels->sum('return_charge')) / ($s_parcels->count() ?: 1), 2) }}
+                        </td>
                     </tr>
                     <tr>
                         <td><strong>Average Order Per Day</strong></td>
-                        <td>327</td>
-                        <td>323</td>
-                        <td>330</td>
+                        <td class="text-end">
+                            {{ number_format(($c_parcels->count() + $s_parcels->count()) / $total_day, 2) }}
+                        </td>
+                        <td class="text-end">{{ number_format($c_parcels->count() / $total_day, 2) }}</td>
+                        <td class="text-end">{{ number_format($s_parcels->count() / $total_day, 2) }}</td>
                     </tr>
                 </tfoot>
             </table>

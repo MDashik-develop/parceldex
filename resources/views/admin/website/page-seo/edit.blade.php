2e311860ed13cc086968a8843028a635d6d7472f
Raja Khan <contact.rajakhan@gmail.com>
2025-02-12 21:19:18 +0600

    `Updated page title and breadcrumb links in admin page-seo edit view`

--- resources/views/admin/website/page-seo/edit.blade.php
+++ resources/views/admin/website/page-seo/edit.blade.php
@@ -5,19 +5,18 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Designation</h1>
+                    <h1 class="m-0 text-dark">Page Seo</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('admin.designation.index') }}">Designations</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.page-seo.index') }}">Page Seo</a></li>
                         <li class="breadcrumb-item active">Edit</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
-
 
     <div class="content">
         <div class="container-fluid">
